1c35547504b5707fcb2bec30021f310cdc5a9e30
Ross <yuefeiduzi@foxmail.com>
2024-06-21 10:46:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.rossmartin.top": dockerHub,
+  "quay.rossmartin.top": "https://quay.io",
+  "gcr.rossmartin.top": "https://gcr.io",
+  "k8s-gcr.rossmartin.top": "https://k8s.gcr.io",
+  "k8s.rossmartin.top": "https://registry.k8s.io",
+  "ghcr.rossmartin.top": "https://ghcr.io",
+  "cloudsmith.rossmartin.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.rossmartin.top": dockerHub,
 };
 
 function routeByHosts(host) {
